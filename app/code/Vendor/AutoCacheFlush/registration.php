d9f1f382715f20121f5a7397a0ce9e9e5157f85c
Vivek singh <122099833+vivek31singh@users.noreply.github.com>
2026-01-13 17:00:54 +0530

    feat: create module skeleton with registration files

---
+++ app/code/Vendor/AutoCacheFlush/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © Vendor. All rights reserved.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Vendor_AutoCacheFlush',
+    __DIR__
+);
